e808682d9909400bac81f66e0031dd2290b08d3e
Ruano12 <mah.benjamim12@gmail.com>
2018-09-13 20:54:21 -0300

    Curso Finalizado.

--- teste-de-unidade/src/br/com/caelum/leilao/servico/GeradorDePagamento.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/GeradorDePagamento.java
@@ -7,17 +7,25 @@ import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Pagamento;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamento;
+import br.com.caelum.leilao.infra.relogio.Relogio;
+import br.com.caelum.leilao.infra.relogio.RelogioDoSistema;
 
 public class GeradorDePagamento {
 	
 	private final RepositorioDeLeiloes leiloes;
 	private final Avaliador avaliador;
 	private final RepositorioDePagamento pagamentos;
+	private final Relogio relogio;
 
-	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamento pagamentos, Avaliador avaliador) {
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamento pagamentos, Avaliador avaliador, Relogio relogio) {
 		this.leiloes = leiloes;
 		this.pagamentos = pagamentos;
 		this.avaliador = avaliador;
+		this.relogio = relogio;
+	}
+	
+	public GeradorDePagamento(RepositorioDeLeiloes leiloes, RepositorioDePagamento pagamentos, Avaliador avaliador) {
+		this(leiloes, pagamentos, avaliador, new RelogioDoSistema());
 	}
 
 	public void gera() {
@@ -26,8 +34,17 @@ public class GeradorDePagamento {
 		for(Leilao leilao : leiloesEncerrados) {
 			this.avaliador.avalia(leilao);
 			
-			Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), Calendar.getInstance());
+			Pagamento novoPagamento = new Pagamento(avaliador.getMaiorLance(), primeiroDiaUtil());
 			this.pagamentos.salva(novoPagamento);
 		}
+	}
+
+	private Calendar primeiroDiaUtil() {
+		Calendar data = relogio.hoje();
+		int diaDaSemana = data.get(Calendar.DAY_OF_WEEK);
+		
+		if(diaDaSemana == Calendar.SATURDAY) data.add(Calendar.DAY_OF_MONTH, 2);
+		else if(diaDaSemana == Calendar.SUNDAY) data.add(Calendar.DAY_OF_MONTH, 1);
+		return data;
 	}
 }
--- teste-de-unidade/src/br/com/caelum/leilao/servico/GeradorDePagamentoTeste.java
+++ teste-de-unidade/src/br/com/caelum/leilao/servico/GeradorDePagamentoTeste.java
@@ -7,6 +7,7 @@ import br.com.caelum.leilao.dominio.Pagamento;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.infra.dao.RepositorioDeLeiloes;
 import br.com.caelum.leilao.infra.dao.RepositorioDePagamento;
+import br.com.caelum.leilao.infra.relogio.Relogio;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -14,6 +15,7 @@ import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
@@ -41,5 +43,35 @@ public class GeradorDePagamentoTeste {
 		Pagamento pagamentoGerado = argumento.getValue();
 				
 		assertEquals(2500.00, pagamentoGerado.getValor(), 0.000001);
+	}
+	
+	@Test
+	public void deveEmpurrarParaOProximoDiaUtil() {
+		RepositorioDeLeiloes leiloes = mock(RepositorioDeLeiloes.class);
+		RepositorioDePagamento pagamentos = mock(RepositorioDePagamento.class);
+		Relogio relogio = mock(Relogio.class);
+		
+		Leilao leilao = new CriadorDeLeilao().para("Xbox")
+				.lance(new Usuario("José da Silva"), 2000)
+				.lance(new Usuario("Maria Pereira"), 2500)
+				.constroi();
+		
+		when(leiloes.encerrados()).thenReturn(Arrays.asList(leilao));
+		
+		Calendar sabado = Calendar.getInstance();
+		sabado.set(2012, Calendar.APRIL, 7);
+		
+		when(relogio.hoje()).thenReturn(sabado);
+		
+		GeradorDePagamento gerador = new GeradorDePagamento(leiloes, pagamentos, new Avaliador(), relogio);
+		gerador.gera();
+		
+		ArgumentCaptor<Pagamento> argumento = ArgumentCaptor.forClass(Pagamento.class);
+		verify(pagamentos).salva(argumento.capture());
+		
+		Pagamento pagamentoGerado = argumento.getValue();
+		
+		assertEquals(Calendar.MONDAY, pagamentoGerado.getData().get(Calendar.DAY_OF_WEEK));
+		assertEquals(9, pagamentoGerado.getData().get(Calendar.DAY_OF_MONTH));
 	}
 }
